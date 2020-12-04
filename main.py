d92fc045cde15e7323b3629cd57d180fc262c0be
miaowei <miaowei@dangdang.com>
2020-12-04 10:56:23 +0800

    update

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ from lxml import etree
 #567
 # def start():
 
+
+
 #     for i in range(1, 21):
 #         page = urllib.request.urlopen(
 #             'http://www.aastocks.com/tc/ipo/ListedIPO.aspx?iid=ALL&orderby=DA&value=DESC&index=%s' % i)
